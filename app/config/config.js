1e08f5d894578b2d1deb8dd24d9334185c51cb7d
Sayem-2021331081 <136493449+Sayem-2021331081@users.noreply.github.com>
2026-03-08 19:35:01 +0600

    Add files via upload

---
+++ app/config/config.js
@@ -0,0 +1,24 @@
+export const PORT=5050
+export const DATABASE='mongodb+srv://sayem:12345@cluster0.jqx4voa.mongodb.net/Mern6TaskApi'
+export const JWT_KEY='ABC12341241234'
+//user kotodin login obostai thakbe
+export const JWT_EXPIRE_TIME=30*24*60*60
+
+export const EMAIL_HOST="smtp.gmail.com"
+export const EMAIL_PORT=587
+export const EMAIL_SECURITY=false
+export const EMAIL_USER="sayem.shuvo999@gmail.com"
+export const EMAIL_PASS="miwb bgaq bevh rfqv"
+export const EMAIL_UN_AUTH=false
+
+export const WEB_CACHE=false
+export const MAX_JSON_SIZE="10MB"
+export const URL_ENCODE=true
+
+export const REQUEST_TIME=20*60*1000
+export const REQUEST_NUMBER=2000
+
+
+
+
+
---
+++ app/model/TasksModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema(
+    {
+        title:{type:String,required: true},
+        description:{type:String,required: true},
+        status:{type:String,required: true},
+        user_id:{type:mongoose.Schema.Types.ObjectId,required:true}//task ta ke banise
+    },
+    {
+        timestamps: true,
+        versionKey:false
+    }
+)
+
+const Tasks = mongoose.model("tasks", TaskSchema);
+
+export default Tasks;
